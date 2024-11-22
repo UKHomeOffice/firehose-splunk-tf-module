06dd3138f245a5e9cca8b951c396106fd3ad1eb9
William Murphy <william.a.murphy@accenture.com>
2024-11-22 14:28:33 +0000

    SE-742 subscription filter for firehose

---
+++ modules/subscription_filters/subscription-filter.tf
@@ -0,0 +1,14 @@
+resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_filter" {
+  for_each = tomap({
+    for log_group_name, details in local.config["log_groups"] : 
+    log_group_name => details
+    if contains(details["accounts"], var.account)
+  })
+  
+  name            = "${each.key}-subscription"
+  log_group_name  = each.key
+  destination_arn = var.firehose_arn
+  filter_pattern  = lookup(each.value, "subscription_filter", " ")
+
+  depends_on = [ data.aws_s3_object.config_file ]
+}
