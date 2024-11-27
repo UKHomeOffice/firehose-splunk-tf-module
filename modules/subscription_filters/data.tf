9a022e22b166748d630c46d0838ff19ba3ff075b
William Murphy <william.a.murphy@accenture.com>
2024-11-27 16:43:31 +0000

    SE-748 remove provder congig

--- modules/subscription_filters/data.tf
+++ modules/subscription_filters/data.tf
@@ -1,20 +1,3 @@
-# Enforce Terraform version and define backend as S3
-terraform {
-  required_version = ">= 0.12.31"
-
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region 
-}
-
-
 data "aws_s3_object" "config_file" {
   bucket = var.config_s3_bucket
   key    = var.config_s3_key
