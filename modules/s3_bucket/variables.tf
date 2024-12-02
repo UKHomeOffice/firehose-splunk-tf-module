b91185818aad7b76d6c844ac1cb1d2bf5975d748
Matthew Halucha <matthew.halucha@accenture.com>
2024-12-02 13:26:32 +0000

    SE-743 Adds s3 bucket policy

--- modules/s3_bucket/variables.tf
+++ modules/s3_bucket/variables.tf
@@ -2,3 +2,9 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
+variable "approved_s3_resources" {
+  description = "List of resource ARNs which can PUT into S3"
+  default     = []
+  type        = list(string)
+}
