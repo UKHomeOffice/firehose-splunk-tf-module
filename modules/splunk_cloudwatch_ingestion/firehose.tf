caa69357d5647fe79791d2e944dac4de886396f2
Thomas Nicholson <thomas.nicholson@accenture.com>
2024-12-03 10:50:26 +0000

    SE-692 Adding firehose log group

--- modules/splunk_cloudwatch_ingestion/firehose.tf
+++ modules/splunk_cloudwatch_ingestion/firehose.tf
@@ -50,8 +50,8 @@ resource "aws_kinesis_firehose_delivery_stream" "kinesis_firehose" {
 
     cloudwatch_logging_options {
       enabled         = var.enable_fh_cloudwatch_logging
-      log_group_name  = "/aws/kinesisfirehose/${local.firehose_stream_name}"
-      log_stream_name = var.log_stream_name
+      log_group_name  = aws_cloudwatch_log_group.firehose_log_group.name
+      log_stream_name = aws_cloudwatch_log_stream.firehose_log_stream.name
     }
   }
 
