011a0a97c84d47f1283a9bd95ca2053cce8769a9
Thomas Nicholson <thomas.nicholson@accenture.com>
2024-12-03 10:43:06 +0000

    SE-692 Adding firehose log group

--- modules/splunk_cloudwatch_ingestion/log_group.tf
+++ modules/splunk_cloudwatch_ingestion/log_group.tf
@@ -1,4 +1,6 @@
 resource "aws_cloudwatch_log_group" "firehose_log_group" {
+  # checkov:skip=CKV_AWS_158: Not enabling encryption for now
+  # checkov:skip=CKV_AWS_338: Ignore retention below 1 year
   name              = "/aws/kinesisfirehose/${local.firehose_stream_name}"
   retention_in_days = var.cloudwatch_log_retention
   tags              = var.tags
