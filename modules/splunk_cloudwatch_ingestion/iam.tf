221105abe30939515959ae458729ce53445bc2c8
Thomas Nicholson <thomas.nicholson@accenture.com>
2024-12-04 09:22:28 +0000

    SE-692 Locking down KMS IAM again

--- modules/splunk_cloudwatch_ingestion/iam.tf
+++ modules/splunk_cloudwatch_ingestion/iam.tf
@@ -56,7 +56,7 @@ data "aws_iam_policy_document" "kinesis_firehose_policy_document" {
 
   statement {
     actions   = ["kms:Decrypt", "kms:GenerateDataKey"]
-    resources = ["*"]
+    resources = [var.s3_kms_key_arn]
     effect    = "Allow"
   }
 
