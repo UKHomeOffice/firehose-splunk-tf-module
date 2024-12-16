1695077843e62b0d530aacd71314d76a40283306
Johnny Che <johnny.che@capgemini.com>
2024-12-16 15:39:51 +0000

    CCL-1340: Update lambda build script

--- lambdas/transformation_lambda/package.sh
+++ lambdas/transformation_lambda/package.sh
@@ -3,9 +3,9 @@
 rm -rf venv_package
 rm -rf package
 mkdir package
-python3.12 -m venv venv_package
+python3 -m venv venv_package
 source ./venv_package/bin/activate
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 deactivate
 cp -r venv_package/lib/python3.*/site-packages/* package/
 cp src/mbtp_splunk_cloudwatch_transformation/handler.py package/
