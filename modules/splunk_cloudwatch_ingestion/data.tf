1ac197ebaf65f490e9e4e68b8078119428d59010
Matthew Halucha <matthew.halucha@accenture.com>
2025-03-17 12:06:31 +0000

    SE-909 Adds CloudWatch alarm for BytesPerSecondLimit, PutRequestsPerSecondLimit, RecordsPerSecondLimit

--- modules/splunk_cloudwatch_ingestion/data.tf
+++ modules/splunk_cloudwatch_ingestion/data.tf
@@ -1,3 +1,12 @@
 locals {
   firehose_stream_name = "${var.environment_prefix_variable}-fh-cw2splunk"
+  firehose_bytes_limit = 5242880
+  firehose_put_requests_limit = 2000
+  firehose_incoming_records_limit = 500000
+
+  multiplier_rate = {
+      "100%" : 1
+      "80%" : 0.8
+  }
+  alarm_description_text = "Alarm when ${local.firehose_stream_name} Firehose reaches"
 }
--- modules/splunk_cloudwatch_ingestion/monitoring.tf
+++ modules/splunk_cloudwatch_ingestion/monitoring.tf
@@ -54,12 +54,8 @@ resource "aws_cloudwatch_metric_alarm" "lambda_throttles_alarm" {
 
 # CloudWatch alarm for SQS Queue Messages
 resource "aws_cloudwatch_metric_alarm" "sqs_message_backlog" {
-  for_each = toset([
-    "${var.environment_prefix_variable}-${var.retry_sqs_name}",
-    "${var.environment_prefix_variable}-${var.retry_dlq_name}"
-  ])
-  alarm_name                = "${each.value}-sqs_message_backlog_alarm"
-  alarm_description         = "Triggers when there are more than 3 messages in the SQS queue."
+  alarm_name                = "${var.environment_prefix_variable}-${var.retry_sqs_name}-sqs_message_backlog_alarm"
+  alarm_description         = "Triggers when there are more than 3 messages in the ${var.environment_prefix_variable}-${var.retry_sqs_name} SQS queue."
   comparison_operator       = "GreaterThanThreshold"
   evaluation_periods        = 3
   metric_name               = "ApproximateNumberOfMessagesVisible"
@@ -70,7 +66,28 @@ resource "aws_cloudwatch_metric_alarm" "sqs_message_backlog" {
   insufficient_data_actions = []
   tags                      = var.tags
   dimensions = {
-    QueueName = each.value
+    QueueName = "${var.environment_prefix_variable}-${var.retry_sqs_name}"
+  }
+  treat_missing_data = "ignore"
+  actions_enabled    = true
+  alarm_actions      = [aws_sns_topic.sns_topic_alerts.arn]
+  ok_actions         = [aws_sns_topic.sns_topic_alerts.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "sqs_dlq_message_backlog" {
+  alarm_name                = "${var.environment_prefix_variable}-${var.retry_dlq_name}-dlq_message_backlog_alarm"
+  alarm_description         = "Triggers when there are messages in the ${var.environment_prefix_variable}-${var.retry_dlq_name} DLQ SQS queue. Navigate to https://${var.region}.console.aws.amazon.com/lambda/home?region=${var.region}#/functions/${var.environment_prefix_variable}-${var.process_failures_lambda_name}?tab=testing, Click Test"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 1
+  metric_name               = "ApproximateNumberOfMessagesVisible"
+  namespace                 = "AWS/SQS"
+  period                    = 60
+  statistic                 = "Sum"
+  threshold                 = 0
+  insufficient_data_actions = []
+  tags                      = var.tags
+  dimensions = {
+    QueueName = "${var.environment_prefix_variable}-${var.retry_dlq_name}"
   }
   treat_missing_data = "ignore"
   actions_enabled    = true
@@ -82,7 +99,7 @@ resource "aws_cloudwatch_metric_alarm" "sqs_message_backlog" {
 # CloudWatch Alarm for Firehose Splunk Processing
 resource "aws_cloudwatch_metric_alarm" "cloudwatch_alarm_firehose_splunk_processing" {
   alarm_name                = "${var.environment_prefix_variable}-firehose_splunk"
-  alarm_description         = "Alarm if the Kinesis Firehose to Splunk stops sending data to Splunk"
+  alarm_description         = "Alarm if the ${local.firehose_stream_name} Firehose to Splunk stops sending data to Splunk"
   metric_name               = "DeliveryToSplunk.Success"
   namespace                 = "AWS/Firehose"
   statistic                 = "Average"
@@ -93,6 +110,81 @@ resource "aws_cloudwatch_metric_alarm" "cloudwatch_alarm_firehose_splunk_process
   insufficient_data_actions = []
   tags                      = var.tags
   treat_missing_data        = "ignore"
+  actions_enabled           = true
+  alarm_actions             = [aws_sns_topic.sns_topic_alerts.arn]
+  ok_actions                = [aws_sns_topic.sns_topic_alerts.arn]
+  dimensions = {
+    DeliveryStreamName = local.firehose_stream_name
+  }
+}
+
+# CloudWatch Alarm for IncomingBytes 
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_alarm_firehose_incoming_bytes_100" {
+  for_each = local.multiplier_rate
+  alarm_name                = "${var.environment_prefix_variable}-firehose-incoming-bytes"
+  alarm_description         = "${alarm_description_text} ${each.key} of the BytesPerSecondLimit"
+  metric_name               = "BytesPerSecondLimit"
+  namespace                 = "AWS/Firehose"
+  statistic                 = "Maximum"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 5
+  datapoints_to_alarm       = 3
+  period                    = 60 
+  threshold                 = local.firehose_bytes_limit * each.value
+  insufficient_data_actions = []
+  tags                      = var.tags
+  treat_missing_data        = "ignore" 
+  actions_enabled           = true
+  alarm_actions             = [aws_sns_topic.sns_topic_alerts.arn]
+  ok_actions                = [aws_sns_topic.sns_topic_alerts.arn]
+  dimensions = {
+    DeliveryStreamName = local.firehose_stream_name
+  }
+}
+
+# CloudWatch Alarm for IncomingPutRequests 
+
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_alarm_firehose_incoming_put_requests" {
+  for_each = local.multiplier_rate
+  alarm_name                = "${var.environment_prefix_variable}-firehose-incoming-put-requests"
+  alarm_description         = "${alarm_description_text} ${each.key} of the PutRequestsPerSecondLimit"
+  metric_name               = "PutRequestsPerSecondLimit"
+  namespace                 = "AWS/Firehose"
+  statistic                 = "Maximum"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 5
+  datapoints_to_alarm       = 3
+  period                    = 60 
+  threshold                 = local.firehose_put_requests_limit * each.value
+  insufficient_data_actions = []
+  tags                      = var.tags
+  treat_missing_data        = "ignore" 
+  actions_enabled           = true
+  alarm_actions             = [aws_sns_topic.sns_topic_alerts.arn]
+  ok_actions                = [aws_sns_topic.sns_topic_alerts.arn]
+  dimensions = {
+    DeliveryStreamName = local.firehose_stream_name
+  }
+}
+
+
+# CloudWatch Alarm for IncomingRecords 
+
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_alarm_firehose_incoming_records" {
+  for_each = local.multiplier_rate
+  alarm_name                = "${var.environment_prefix_variable}-firehose-incoming-records"
+  alarm_description         = "${alarm_description_text} ${each.key} of the RecordsPerSecondLimit"
+  metric_name               = "RecordsPerSecondLimit"
+  namespace                 = "AWS/Firehose"
+  statistic                 = "Maximum"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 5
+  datapoints_to_alarm       = 3
+  period                    = 60 
+  threshold                 = local.firehose_incoming_records_limit * each.value
+  insufficient_data_actions = []
+  tags                      = var.tags
+  treat_missing_data        = "ignore" 
   actions_enabled           = true
   alarm_actions             = [aws_sns_topic.sns_topic_alerts.arn]
   ok_actions                = [aws_sns_topic.sns_topic_alerts.arn]
