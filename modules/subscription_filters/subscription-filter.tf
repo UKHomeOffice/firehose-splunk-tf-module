a82b703c4d5438da37521166c73404a074a07535
Thomas Nicholson <Thomas.Nicholson@digital.homeoffice.gov.uk>
2025-03-28 08:34:03 +0000

    SE-943 Removed log group name from subscription name

--- modules/subscription_filters/subscription-filter.tf
+++ modules/subscription_filters/subscription-filter.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_log_filter" {
   for_each = tomap({
-    for log_group_name, details in local.config["log_groups"] :
+    for log_group_name, details in local.config.log_groups :
     log_group_name => details
     if contains(details["accounts"], tonumber(var.account_id))
   })
 
-  name            = "${each.key}-${each.value.log_group}-subscription"
+  name            = "${each.key}-subscription"
   log_group_name  = each.value.log_group
   destination_arn = var.firehose_arn
   filter_pattern  = lookup(each.value, "subscription_filter", " ")
